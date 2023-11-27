55d8298deaed394cc5fc371bc6b78360a8d84c7d
Biswajit Sundara <biswajitsundara@gmail.com>
2023-11-27 23:56:04 +0530

    search api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+process.env.REACT_APP_GOOGLE_API_KEY
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
-
+export const VIDEO_SEARCH_API = "https://youtube.googleapis.com/youtube/v3/search?type=videos&part=snippet&q=hello&key="+process.env.REACT_APP_GOOGLE_API_KEY
