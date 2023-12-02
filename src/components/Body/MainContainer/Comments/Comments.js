7e44828c505d4b3bf7b8191c9f643e7ba8d179c3
Biswajit Sundara <biswajitsundara@gmail.com>
2023-12-03 00:10:00 +0530

    comments

--- src/components/Body/MainContainer/Comments/Comments.js
+++ src/components/Body/MainContainer/Comments/Comments.js
@@ -1,8 +1,17 @@
 const Comments = ({ data }) => {
+  const { name, text, replies } = data;
+
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.text}</p>
+    <div className="flex flex-row shadow-sm bg-gray-100">
+      <img
+        className="w-12 h-12"
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        alt="user"
+      />
+      <div className="px-3">
+        <h1>{name}</h1>
+        <p>{text}</p>
+      </div>
     </div>
   );
 };
